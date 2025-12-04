f121f785b1a1e2966c768b1b0860970af682aec7
Magali Rosales <milydt157@gmail.com>
2025-12-04 16:01:32 -0600

    vista coordy y estudiante

--- app/src/main/java/com/example/tutorias/MainActivity.kt
+++ app/src/main/java/com/example/tutorias/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.tutorias
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val etUsuario = findViewById<EditText>(R.id.etUsuario)
+        val btnAcceder = findViewById<Button>(R.id.btnAcceder)
+
+        btnAcceder.setOnClickListener {
+            val usuario = etUsuario.text.toString().trim()
+
+            when (usuario) {
+                "coordi" -> {
+                    startActivity(Intent(this, coordi::class.java))
+                }
+                "2025004" -> {
+                    startActivity(Intent(this, inicio_estudiante::class.java))
+                }
+
+                else -> {
+                    Toast.makeText(this, "Usuario no válido", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
